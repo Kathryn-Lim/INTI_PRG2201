46bb3c784e4f40d624cfa7fd65527e4f18f9b702
Kathryn <63330154+Kathryn-Lim@users.noreply.github.com>
2021-08-06 14:50:54 +0800

    Add files via upload

---
+++ Intro1.java
@@ -0,0 +1,5 @@
+class Intro1 {
+    public static void main(String[] args) {
+        System.out.println("My name is Kathryn.\nMy hobbies are music, electronics and watching videos.\nMy favorite quote is \"happiness is experienced not pursued\" ");
+    }
+}
